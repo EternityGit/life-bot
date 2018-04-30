380575f09fc85046df345627aca7cb665942d372
Eternity <38627086+EternityGit@users.noreply.github.com>
2018-04-30 16:40:21 +0200

    Add files via upload

---
+++ needs/food.js
@@ -0,0 +1,12 @@
+class Food {
+    static getMorningFoodMessage() {
+        return 'Bonjour @everyone, c\'est l\'heure de ma ration de croquette du matin :smirk_cat.'
+    }
+
+    static getEveningFoodMessage() {
+        return 'Bonjour @everyone, c\'est l\'heure de ma ration de croquette du soir :smirk_cat.'
+    }
+}
+
+
+
---
+++ needs/treatment.js
@@ -0,0 +1,9 @@
+class Treatment {
+    static getFleaTreatmentMessage() {
+        return 'Hey @everyone, c\'est l\'heure de me donner mon traitement anti-puce. :smirk_cat:'
+    }
+
+    static getOilTreatmentMessage() {
+        return 'Hey @everyone, c\'est l\'heure de me donner mon soin d\'huile essentiel. :smirk_cat:'
+    }
+}
