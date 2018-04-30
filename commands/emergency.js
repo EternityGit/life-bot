0a99c1e2d29802cbb45b38acbd97a80e280eda12
Eternity <38627086+EternityGit@users.noreply.github.com>
2018-04-30 16:41:04 +0200

    Update emergency.js

--- commands/emergency.js
+++ commands/emergency.js
@@ -7,9 +7,9 @@ module.exports = class Emergency extends Command {
     }
 
     static action(message) {
-        let emergency =   '@everyone CONTACT :\nLe maître : **@Entropy#0559**\n' +
+        let emergency =   '@everyone CONTACT :\n\nLe maître : **@Entropy#0559**\n\n' +
                             'La clinique vétérinaire actuelle :\n' +
-                            '**Clinique Vétérinaire de la Vallée**\n86 Rue du Général Leclerc, 95120 Ermont\n01 34 14 40 40\n' +
+                            '**Clinique Vétérinaire de la Vallée**\n86 Rue du Général Leclerc, 95120 Ermont\n01 34 14 40 40\n\n' +
                             '**============== EN CAS D\'URGENCES ==============**\n' +
                             '**==** https://www.vetoadom.com/veterinaire-de-garde/95 **==**\n' +
                             '**============================================**'
