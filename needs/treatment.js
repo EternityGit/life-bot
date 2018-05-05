464c22024baf2b7b094faa263db281448fca79d4
Eternity <38627086+EternityGit@users.noreply.github.com>
2018-05-05 21:30:24 +0200

    Update treatment.js

--- needs/treatment.js
+++ needs/treatment.js
@@ -1,9 +1,8 @@
-class Treatment {
-    static getFleaTreatmentMessage() {
-        return 'Hey @everyone, c\'est l\'heure de me donner mon traitement anti-puce. :smirk_cat:'
-    }
-
-    static getOilTreatmentMessage() {
-        return 'Hey @everyone, c\'est l\'heure de me donner mon soin d\'huile essentiel. :smirk_cat:'
+module.exports = class Treatment {
+    static getFleaTreatmentMessage(product) {
+        if (product == "Broadline") {
+            return 'Hey @everyone, c\'est l\'heure de me donner mon traitement anti-puce.\n\t**' + product + '**\n:smirk_cat:'
+        }
+        return 'Hey @everyone, c\'est l\'heure de me donner mon soin d\'huile essentiel\n\t**' + product + '**.\n:smirk_cat:'
     }
 }
